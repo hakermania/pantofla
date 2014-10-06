b103c2e808a589d11ea8945f561a86e25e1379f9
Alex Solanos <alexsol.developer@gmail.com>
2014-10-07 00:35:28 +0300

    Removed output and simplemath from root

--- output.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-
-def stdout(message, prefix=""):
-	sys.stdout.write(prefix+message+"\n")
-
-def stderr(message, prefix="Error: "):
-	sys.stderr.write(prefix+message+"\n")
